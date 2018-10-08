9a4c6ae18e2aac0fc90b2cff8c3c4fdf8f269ef6
siddhant1 <sidhant.manchanda@gmail.com>
2018-10-08 23:19:59 +0530

    Completed Using Styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import styled, { css } from "styled-components";
+import styled, { css, ThemeProvider } from "styled-components";
 
 const mainColor = "#9D79BC";
 
@@ -37,8 +37,21 @@ const Thumbnail = styled.img`
   ${border};
 `;
 
+const Wrapper = styled.div`
+  background-color: ${props => props.theme.main};
+`;
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: {
+        main: "ghostwhite"
+      }
+    };
+  }
+
   render() {
+    console.log(this.props);
     const thumbnails = Array.from({ length: 4 }, (_, index) => {
       const showBorder = index % 2 === 0;
       return (
@@ -51,10 +64,25 @@ class App extends Component {
     });
 
     return (
-      <div className={this.props.className}>
-        <Title color={mainColor}>Compustagram</Title>
-        <Gallery>{thumbnails}</Gallery>
-      </div>
+      <ThemeProvider theme={this.state.theme}>
+        <Wrapper className={this.props.className}>
+          <Title color={mainColor}>Compustagram</Title>
+          <Gallery>{thumbnails}</Gallery>
+          Change Themes :-
+          <select
+            onChange={event => {
+              this.setState({ theme: { main: event.target.value } });
+            }}
+          >
+            <option value="ghostwhite">Light</option>
+            <option value="#3A3E3B">Dark</option>
+            <option value="#EFBCD5">Pink</option>
+            <option value="#0F8B8D">Blue</option>
+            <option value="#E54B4B">Red</option>
+            <option value="#43C59E">Green</option>
+          </select>
+        </Wrapper>
+      </ThemeProvider>
     );
   }
 }
@@ -64,3 +92,5 @@ export default styled(App)`
   height: 100vh;
   text-align: center;
 `;
+
+// Note that when you style a component, you need to make sure that your component has this.props.className attached to its DOM. Because after Style-Components generated a unique class name, it will pass the class name to your component. If you are not sure whether a third party component has this.props.className or not, you can simply wrap it up in your own <div className={this.props.className}> tag.
