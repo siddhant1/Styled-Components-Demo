fd5b87256bde510a41a4d8d2638d0eee9dd65475
siddhant1 <sidhant.manchanda@gmail.com>
2018-10-08 22:14:17 +0530

    WIP initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled, { ThemeProvider } from "styled-components";
 
+const Header = styled.div`
+  color: ${props => props.theme.main || "pink"};
+`;
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: {
+        main: "black"
+      }
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <ThemeProvider theme={this.state.theme}>
+        <div className="App">
+          <Header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </Header>
+          <select
+            onChange={event => {
+              this.setState({ theme: { main: event.target.value } });
+            }}
           >
-            Learn React
-          </a>
-        </header>
-      </div>
+            <option value="red">RED</option>
+            <option value="yellow">Yellow</option>
+            <option value="pink">Pink</option>
+            <option value="mediumseagreen">Green</option>
+          </select>
+        </div>
+      </ThemeProvider>
     );
   }
 }
